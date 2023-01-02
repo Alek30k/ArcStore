d55b52773dcb028f0a62a018228945499ae234d9
Alek30k <alemank23k@gmail.com>
2023-01-02 20:31:42 -0300

    last commit

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -23,15 +23,29 @@ const Products = () => {
           <h2>Filter by price</h2>
           <div className="inputItem">
             <span>0</span>
-            <input type="range" />
+            <input type="range" min={0} max={100} />
             <span>1000</span>
           </div>
         </div>
         <div className="filterItem">
           <h2>Sort by</h2>
+          <div className="inputItem">
+            <input type="radio" id="asc" value="asc" name="price" />
+            <label htmlFor="asc">Price (Lowest first)</label>
+          </div>
+          <div className="inputItem">
+            <input type="radio" id="desc" value="desc" name="price" />
+            <label htmlFor="desc">Price (Highest first)</label>
+          </div>
         </div>
       </div>
-      <div className="right"></div>
+      <div className="right">
+        <img
+          className="catImg"
+          src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600"
+          alt=""
+        />
+      </div>
     </div>
   );
 };
