9d2289ece059170c60875d5e250a610c1cc52cae
Alek30k <alemank23k@gmail.com>
2023-01-02 20:21:55 -0300

    last commit

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -8,11 +8,24 @@ const Products = () => {
           <h2>Product Categories</h2>
           <div className="inputItem">
             <input type="checkbox" id="1" value={1} />
-            <label htmlFor="1">1</label>
+            <label htmlFor="1">Shoes</label>
+          </div>
+          <div className="inputItem">
+            <input type="checkbox" id="2" value={2} />
+            <label htmlFor="2">Skirt</label>
+          </div>
+          <div className="inputItem">
+            <input type="checkbox" id="3" value={3} />
+            <label htmlFor="3">Coats</label>
           </div>
         </div>
         <div className="filterItem">
           <h2>Filter by price</h2>
+          <div className="inputItem">
+            <span>0</span>
+            <input type="range" />
+            <span>1000</span>
+          </div>
         </div>
         <div className="filterItem">
           <h2>Sort by</h2>
