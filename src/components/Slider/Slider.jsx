d3071c6ac00f31805c65a961c7fc24eed40a278f
Alek30k <alemank23k@gmail.com>
2023-01-02 14:05:39 -0300

    last commit

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -21,16 +21,19 @@ const Slider = () => {
 
   return (
     <div className="slider">
-      <div className="container">
+      <div
+        className="container"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
         <img src={data[0]} alt="" />
         <img src={data[1]} alt="" />
         <img src={data[2]} alt="" />
       </div>
       <div className="icons">
-        <div className="icon">
+        <div className="icon" onClick={prevSlide}>
           <WestOutlinedIcon />
         </div>
-        <div className="icon">
+        <div className="icon" onClick={nextSlide}>
           <EastOutlinedIcon />
         </div>
       </div>
