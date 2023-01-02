176ba4984b65a2a466bc1f1024795b93d16d62b9
Alek30k <alemank23k@gmail.com>
2023-01-02 19:35:55 -0300

    last commit

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,7 +1,26 @@
 import "./Products.scss";
 
 const Products = () => {
-  return <div>Products</div>;
+  return (
+    <div>
+      <div className="left">
+        <div className="filterItem">
+          <h2>Product Categories</h2>
+          <div className="inputItem">
+            <input type="checkbox" id="1" value={1} />
+            <label htmlFor="1">1</label>
+          </div>
+        </div>
+        <div className="filterItem">
+          <h2>Filter by price</h2>
+        </div>
+        <div className="filterItem">
+          <h2>Sort by</h2>
+        </div>
+      </div>
+      <div className="right"></div>
+    </div>
+  );
 };
 
 export default Products;
