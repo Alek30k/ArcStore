4034fab143d80316e74d10a20c6b3f8e42e94b82
Alek30k <alemank23k@gmail.com>
2023-01-02 21:46:30 -0300

    last commit

---
+++ src/components/List/List.jsx
@@ -0,0 +1,54 @@
+import Card from "../Card/Card";
+import "./List.scss";
+
+const List = () => {
+  const data = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "sgdahshdgjasghdjh",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "sgdahshdgjasghdjh",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "sgdahshdgjasghdjh",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 4,
+      img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "sgdahshdgjasghdjh",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+  ];
+
+  return (
+    <div className="list">
+      {data?.map((item) => (
+        <Card item={item} key={item.id} />
+      ))}
+      }
+    </div>
+  );
+};
+
+export default List;
