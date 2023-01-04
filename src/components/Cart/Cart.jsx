b27bfa39908a725ef352b46c5f46b79917466037
Alek30k <alemank23k@gmail.com>
2023-01-04 15:37:11 -0300

    last commit

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,44 @@
+import "./Cart.scss";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+
+const Cart = () => {
+  const data = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "sgda hshdgj asghdjh",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "sgdahshdgjasghdjh",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+  ];
+
+  return (
+    <div className="cart">
+      <h1>Products in your cart</h1>
+      {data?.map((item) => (
+        <div className="item" key={item.id}>
+          <img src={item.img} alt="" />
+          <div className="details">
+            <h1>{item.title}</h1>
+            <p>{item.desc.substring(0, 100)}</p>
+            <div className="price">1 x ${item.price}</div>
+          </div>
+        </div>
+      ))}
+      <DeleteOutlinedIcon className="dalete" />
+    </div>
+  );
+};
+
+export default Cart;
