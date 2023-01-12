d545ff6b4dd6d8565df78d09fdc7610d5f904b91
Alek30k <alemank23k@gmail.com>
2023-01-11 22:09:55 -0300

    last commit

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { removeItem, resetCart } from "../../redux/cartReducer";
 import { makeRequest } from "../../makeRequest";
 import { loadStripe } from "@stripe/stripe-js";
+import { useState } from "react";
 
 const Cart = () => {
   const products = useSelector((state) => state.cart.products);
@@ -36,32 +37,34 @@ const Cart = () => {
   };
 
   return (
-    <div className="cart">
-      <h1>Products in your cart</h1>
-      {products?.map((item) => (
-        <div className="item" key={item.id}>
-          <img src={process.env.REACT_APP_UPLOAD_URL + item.img} alt="" />
-          <div className="details">
-            <h1>{item.title}</h1>
-            <p>{item.desc?.substring(0, 100)}</p>
-            <div className="price">
-              {item.quantity} x ${item.price}
+    <div className="ccontainer">
+      <div className="cart">
+        <h1>Products in your cart</h1>
+        {products?.map((item) => (
+          <div className="item" key={item.id}>
+            <img src={process.env.REACT_APP_UPLOAD_URL + item.img} alt="" />
+            <div className="details">
+              <h1>{item.title}</h1>
+              <p>{item.desc?.substring(0, 100)}</p>
+              <div className="price">
+                {item.quantity} x ${item.price}
+              </div>
             </div>
+            <DeleteOutlinedIcon
+              className="delete"
+              onClick={() => dispatch(removeItem(item.id))}
+            />
           </div>
-          <DeleteOutlinedIcon
-            className="delete"
-            onClick={() => dispatch(removeItem(item.id))}
-          />
+        ))}
+        <div className="total">
+          <span>SUBTOTAL</span>
+          <span>${totalPrice()}</span>
         </div>
-      ))}
-      <div className="total">
-        <span>SUBTOTAL</span>
-        <span>${totalPrice()}</span>
+        <button onClick={handlePayment}>PROCEED TO CHECKOUT</button>
+        <span className="reset" onClick={() => dispatch(resetCart())}>
+          Reset Cart
+        </span>
       </div>
-      <button onClick={handlePayment}>PROCEED TO CHECKOUT</button>
-      <span className="reset" onClick={() => dispatch(resetCart())}>
-        Reset Cart
-      </span>
     </div>
   );
 };
